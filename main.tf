d33cdfe4b20599116c6e2d37fcd52ed71f87c76e
Harshahd97 <154952682+Harshahd97@users.noreply.github.com>
2024-03-04 17:03:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,8 @@
-provider "aws" {
-  region = var.region
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr_block
-
-  tags = {
-    Name = "Myvpc"
-  }
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-resource "aws_subnet" "examples" {
-  count                  = length(data.aws_availability_zones.available.names)
-  vpc_id                 = aws_vpc.main.id
-  cidr_block             = values(var.subnet_cidr_blocks)[count.index]
-  availability_zone      = data.aws_availability_zones.available.names[count.index]
-
-tags = {
-  Name = "${var.tag_name}${count.index + 1}"
-  }
-}
-
-output "subnetids" {
-  value       = aws_subnet.examples.*.id
-  description = "The subnets created were"
+ tags = {
+   Name = var.name_tag,
+ }
 }
